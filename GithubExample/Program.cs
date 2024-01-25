51f5debb1a949fb771d0d1f71848dea7309bc5fc
NivruttiJadhav <jadhavnivrutti189@gmail.com>
2024-01-25 14:39:48 +0530

    Add project files.

---
+++ GithubExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            add();
+        }
+
+        public static void add()
+        {
+            int x = 10;
+            int y = 10;
+            int z = x + y;
+            Console.WriteLine(z);
+
+
+        }
+    }
+}
